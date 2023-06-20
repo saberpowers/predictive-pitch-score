2498f2c3093ee255201c808f5ebb2dbe2a32f8d7
Scott Powers <saberpowers@gmail.com>
2023-06-19 21:22:00 -0500

    initial pitch outcome model for binary outcome tree

--- package/predpitchscore/R/get_outcome_tree.R
+++ package/predpitchscore/R/get_outcome_tree.R
@@ -6,9 +6,11 @@ get_outcome_tree <- function(call_code) {
       "Foul", "Foul Bunt", "Foul Tip", "In play, no out", "In play, out(s)", "In play, run(s)",
       "Missed Bunt", "Swinging Strike", "Swinging Strike (Blocked)"
     ),
-    is_hit_by_pitch = call_code %in% c("Hit by Pitch"),
-    is_called_strike = call_code %in% c("Called Strike"),
+    is_hbp = call_code %in% c("Hit by Pitch"),
+    is_strike = call_code %in% c("Called Strike"),
     is_contact = call_code %in% c("Foul", "In play, no out", "In play, out(s)", "In play, run(s)"),
     is_fair = call_code %in% c("In play, no out", "In play, out(s)", "In play, run(s)")
   )
+
+  return(outcome_tree)
 }
