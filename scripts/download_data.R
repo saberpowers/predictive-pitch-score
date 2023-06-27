5beb843fa484d094fdc5142d6c81c5e98a080b29
Scott Powers <saberpowers@gmail.com>
2023-06-27 14:43:35 -0500

    point ETL directly to statsapi

--- scripts/download_data.R
+++ scripts/download_data.R
@@ -1,7 +1,35 @@
 
-library(predpitchscore)
+# Parse command line options ----
 
-year <- 2016
-data_dir <- "data"
-extract_season(year = year, data_dir = data_dir)
-consolidate_data(year = year, data_dir = data_dir)
+opt_list <- list(
+  optparse::make_option(c("-s", "--start_year"), type = "integer", default = 2022),
+  optparse::make_option(c("-e", "--end_year"), type = "integer", default = 2022),
+  optparse::make_option(c("-d", "--data_dir"), type = "character", default = "data"),
+  optparse::make_option(c("-p", "--parallel"), action = "store_true", default = FALSE),
+  optparse::make_option(c("-v", "--verbose"), action = "store_true", default = FALSE)
+)
+opt <- optparse::OptionParser(option_list = opt_list) |>
+  optparse::parse_args()
+
+
+# Download data ----
+
+cluster <- NULL
+if (opt$parallel) {
+  cluster <- parallel::makeCluster(parallel::detectCores())
+}
+
+for (year in seq(from = opt$start_year, to = opt$end_year)) {
+
+  if (opt$verbose) {
+    logger::log_info("Downloading {year}")
+  }
+
+  data <- predpitchscore::extract_season(year = year, cl = cluster)
+  write.csv(data$event, file = glue::glue("{opt$data_dir}/event/{year}.csv"))
+  write.csv(data$pitch, file = glue::glue("{opt$data_dir}/pitch/{year}.csv"))
+}
+
+if (opt$parallel) {
+  parallel::stopCluster(cluster)
+}
