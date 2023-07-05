6bf88b14abf73db5395b5bc967cfea50f845283e
Scott Powers <saberpowers@gmail.com>
2023-07-04 20:34:42 -0500

    add data loading to estimate_pitch_distribution.R

--- scripts/estimate_pitch_distribution.R
+++ scripts/estimate_pitch_distribution.R
@@ -1,6 +1,9 @@
 
 library(predpitchscore)
 
+pitch <- data.table::fread("data/pitch/2017.csv")
+event <- data.table::fread("data/event/2017.csv")
+
 pitch_distribution_model <- list()
 
 pitch_distribution_model[["FF"]] <- optimize_pitch_posterior(
