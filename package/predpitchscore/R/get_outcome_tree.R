8a76c82584014055f961d8ea951a00c5144c3fc5
vaiglesias6 <132905515+vaiglesias6@users.noreply.github.com>
2023-07-24 11:51:48 -0400

    Fix HBP bug in pitch outcome model (#12)

--- package/predpitchscore/R/get_outcome_tree.R
+++ package/predpitchscore/R/get_outcome_tree.R
@@ -13,7 +13,7 @@ get_outcome_tree <- function(description) {
       "Foul", "Foul Bunt", "Foul Tip", "In play, no out", "In play, out(s)", "In play, run(s)",
       "Missed Bunt", "Swinging Strike", "Swinging Strike (Blocked)"
     ),
-    is_hbp = description %in% c("Hit by Pitch"),
+    is_hbp = description %in% c("Hit By Pitch"),
     is_strike = description %in% c("Called Strike"),
     is_contact = description %in% c("Foul", "In play, no out", "In play, out(s)", "In play, run(s)"),
     is_fair = description %in% c("In play, no out", "In play, out(s)", "In play, run(s)")
